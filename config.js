0ccb47cfe28ae47ae31a3d4fd722e8aa94eeccdc
ToastedSploits <65846161+toasty2436@users.noreply.github.com>
2023-10-12 23:23:23 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://jellyprox.ryanlepley84.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ODc3YmEzNjQ1YjM2MDJiZjYwMDBjMmM2MjA0ZWNkNyIsInN1YiI6IjY1MjdmYTgzODEzODMxMDEzOTBlN2UyMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M_mU1r2y5qj8wSZnrH5NInccTUqEwsGKSjvn4DYPWHI"
+};
